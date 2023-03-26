09f813f4552477b56ac4a3ac0bc17102b160192d
Edgar Méndez <mdzredgar@gmail.com>
2023-03-25 20:37:41 -0500

    Instalación de CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
+const { json } = require('express')
 
+const app = express()
+const PORT = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'Server OK'})
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`)
+})
 
 module.exports = app
