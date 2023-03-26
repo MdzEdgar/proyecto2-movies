2500254d69c9fb04be3fbb81dc684ddda4138068
Edgar Méndez <mdzredgar@gmail.com>
2023-03-25 23:23:47 -0500

    Agregadas mis peliculas

--- src/movies/movies.controllers.js
+++ src/movies/movies.controllers.js
@@ -1,10 +1,24 @@
 const moviesDB = [
   {
-    "id" :1,
-    "title": "Pacific Rim",
-    "description": "Lorem Ipsum",
-    "year": 2012,
-    "director": "Guillermo del Toro"
+    id: 1,
+    "title": "Taxi Driver",
+    "description": "Travis, an ex-marine and Vietnam veteran, works as a taxi driver in New York City. One day, he decides to save an underage prostitute from her pimp in an effort to clean the city of its corruption.",
+    "year": 1976,
+    "director": "Martin Scorsese"
+  },
+  {
+    id: 2,
+    "title": "Fight Club",
+    "description": "Unhappy with his capitalistic lifestyle, a white-collared insomniac forms an underground fight club with Tyler, a careless soap salesman. Soon, their venture spirals down into something sinister.",
+    "year": 1999,
+    "director": "David Fincher"
+  },
+  {
+    id: 3,
+    "title": "American Psycho",
+    "description": "Patrick Bateman, a wealthy investment banker, hides his psychopathic ego from his friends. Later, his illogical fantasies escalate and he submits to an uncontrollable bloodlust.",
+    "year": 2000,
+    "director": "Mary Harron"
   }
 ]
 
